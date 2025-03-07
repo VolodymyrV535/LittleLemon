cf6f2682a849592a2e15f9684dbfe6cbb48ebc13
Volodymyr Velichko <vova.box.new@gmail.com>
2025-03-07 18:20:27 +0200

    connected to MySQL and added 2 models: Booking and Menu

--- littlelemon/restaurant/models.py
+++ littlelemon/restaurant/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone  # Make sure this line is included
+
 
 # Create your models here.
+class Booking(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    bookings_date  = models.DateTimeField(default=timezone.now)
+    
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price =  models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
